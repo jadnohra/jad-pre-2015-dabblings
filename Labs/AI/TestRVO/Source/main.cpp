0970adfe803055ece040b3da984ba6454bb28ec3
pinkfish <pinkfish@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-10-05 23:00:12 +0000

    Added timers, smoothed decoupled rendering, checks for vsync settings (seems not to be working).

--- Labs/AI/TestRVO/Source/main.cpp
+++ Labs/AI/TestRVO/Source/main.cpp
@@ -23,7 +23,13 @@ bool InitVideo(int width = 640, int height = 480, unsigned int flags = SDL_ANYFO
 	}
 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	
+	
 	SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, 1); 
+	// This does not seem to work, only the driver settings are usually used...
+	int check2;
+	int check3 = SDL_GL_GetAttribute( SDL_GL_SWAP_CONTROL, &check2 );
+	
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 4 );
 	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
@@ -57,6 +63,78 @@ bool InitVideo(int width = 640, int height = 480, unsigned int flags = SDL_ANYFO
 	return true;
 }
 
+class Timer
+{
+public:
+
+	unsigned int sdl_time;
+	unsigned int frame_time;
+	unsigned int frame_index;
+	unsigned int rest_time;
+	unsigned int delta_frame_index;
+	float delta_time;
+	float last_returned_time;
+
+	void Start(unsigned int framesPerSec)
+	{
+		frame_time = 1000 / framesPerSec;
+		sdl_time = SDL_GetTicks();
+		frame_index = 0;
+		rest_time = 0;
+		delta_frame_index = 0;
+		delta_time = 0.0f;
+		last_returned_time = 0.0f;
+	}
+
+	bool Update()
+	{
+		unsigned int new_sdl_time = SDL_GetTicks();
+		unsigned int elapsed = new_sdl_time - sdl_time;
+
+		if (elapsed >= frame_time)
+		{
+			delta_frame_index = elapsed / frame_time;
+			frame_index += delta_frame_index;
+			rest_time = elapsed - (delta_frame_index * frame_time);
+
+			sdl_time += delta_frame_index * frame_time;
+
+			return true;
+		}
+
+		return false;
+	}
+
+	float GetFrameLockedTime()
+	{
+		return (float) (frame_index * frame_time) / 1000.0f;
+	}
+
+	float GetTime()
+	{
+		float curr_time = (GetFrameLockedTime() + ((float) rest_time / 1000.0f));
+
+		delta_time = last_returned_time - curr_time;
+		last_returned_time = curr_time;
+		return curr_time;
+	}
+
+	float GetDeltaTime()
+	{
+		return delta_time;
+	}
+
+	unsigned int GetDeltaFrameIndex()
+	{
+		return delta_frame_index;
+	}
+
+	float GetFrameTime()
+	{
+		return (float) (frame_time) / 1000.0f;
+	}
+};
+
 class Object
 {
 public:
@@ -69,9 +147,13 @@ class MovingAgent
 {
 public:
 
+	float t;
 	float pos[2];
 	float radius;
 	float vel[2];
+
+	float last_pos[2];
+	float last_t;
 
 	MovingAgent()
 	{
@@ -80,17 +162,39 @@ public:
 
 		radius = 30.0f;
 
-		vel[0] = 0.0f;
-		vel[1] = 0.0f;
+		vel[0] = 350.0f;
+		vel[1] = 300.0f;
 	}
 
 	virtual void Update(float time, float dt) 
 	{
-		pos[0] += 200.0f * dt;
+		last_t = t;
+		last_pos[0] = pos[0];
+		last_pos[1] = pos[1];
+
+		t = time;
+		pos[0] += vel[0] * dt;
+		pos[1] += vel[1] * dt;
+		
+		if (pos[0] > 640.0f)
+			vel[0] = -vel[0];
+		else if (pos[0] < 0.0f)
+			vel[0] = -vel[0];
+
+		if (pos[1] > 480.0f)
+			vel[1] = -vel[1];
+		else if (pos[1] < 0.0f)
+			vel[1] = -vel[1];
 	}
 	
-	virtual void Draw() 
+	virtual void Draw(float time) 
 	{
+		float interp_pos[2];
+
+		float lerp = (time - last_t) / (t - last_t);
+		interp_pos[0] = last_pos[0] + lerp * (pos[0] - last_pos[0]);
+		interp_pos[1] = last_pos[1] + lerp * (pos[1] - last_pos[1]);
+
 		glColor4f(1.0, 1.0, 1.0, 0.75f);
 		glLineWidth(2.0f);
 		glBegin(GL_LINE_LOOP);
@@ -100,7 +204,7 @@ public:
 		
 		for (float angle = 0.0f; angle <= 2.0f * MATH_PIf; angle += (2.0f * MATH_PIf) / 32.0f)
 		{
-			glVertex2f(pos[0] + sinf(angle) * radius, pos[1] + cosf(angle) * radius);
+			glVertex2f(interp_pos[0] + sinf(angle) * radius, interp_pos[1] + cosf(angle) * radius);
 		}
 
 		glEnd();
@@ -108,44 +212,6 @@ public:
 };
 
 
-/*
-float x1 = 10.0f;
-float y1 = 10.0f;
-float x2 = 40.0f;
-float y2 = 40.0f;
-*/
-
-void Update(float time, float dt)
-{
-	/*
-	float diff = 40.0f * dt;
-
-	x1 += diff;
-	y1 += diff;
-	x2 += diff;
-	y2 += diff;
-	*/
-}
-
-/*
-void Draw(float adjustTime)
-{
-	//printf("%f\n", x1);
-
-	
-
-	glColor3f(1.0, 0.0, 0.0);
-	glBegin(GL_QUADS);
-
-	float dd = 50.1f * adjustTime;
-	dd = 0.0f;
-
-	glVertex2f(x1 + dd, y1 + dd); glVertex2f(x2 + dd, y1 + dd); glVertex2f(x2 + dd, y2 + dd); glVertex2f(x1 + dd, y2 + dd);
-	glEnd();
-
-	
-}
-*/
 
 int main(int argc, char *argv[])
 {
@@ -168,13 +234,52 @@ int main(int argc, char *argv[])
 
 	MovingAgent agent;
 
+	sdl_time_millis = SDL_GetTicks();
+
+	Timer renderTimer;
+	Timer updateTimer;
+
+	renderTimer.Start(60);
+	updateTimer.Start(30);
+
+
 	while (is_running)
 	{
-		unsigned int elapsed_millis  = SDL_GetTicks() - sdl_time_millis;
-		
+		if (renderTimer.Update())
+		{
+			//printf("%f\n", renderTimer.GetTime());
 
+			glClearColor(0.0, 0.0, 0.0, 1.0f);
+			glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			agent.Draw(renderTimer.GetTime());
+			SDL_GL_SwapBuffers();
+		}
+
+		if (updateTimer.Update())
+		{
+			unsigned int frames = updateTimer.GetDeltaFrameIndex();
+
+			for (unsigned int i = 0; i < frames; ++i)
+			{
+				agent.Update(updateTimer.GetFrameLockedTime(), updateTimer.GetFrameTime());
+			}
+		}
+
+		SDL_Delay(1);
+
+		/*
+		unsigned int ticks = SDL_GetTicks();
+		unsigned int elapsed_millis  = ticks - sdl_time_millis;
+		
 		if (elapsed_millis >= frame_tick_millis)
 		{
+			glClearColor(0.0, 0.0, 0.0, 1.0f);
+			glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			agent.Draw();
+			SDL_GL_SwapBuffers();
+
+			//printf("%d\n", elapsed_millis);
+
 			while (elapsed_millis >= frame_tick_millis)
 			{
 				elapsed_millis -= frame_tick_millis;
@@ -182,7 +287,6 @@ int main(int argc, char *argv[])
 				float new_time = ((float) time_millis) / 1000.0f;
 
 				agent.Update(new_time, frame_tick_secs);
-				Update(new_time, frame_tick_secs);
 				
 				time = new_time;
 
@@ -190,15 +294,43 @@ int main(int argc, char *argv[])
 				++frame_count;
 			}
 
-			glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			agent.Draw();
-			SDL_GL_SwapBuffers();
-			//Draw(((float) elapsed_millis) / 1000.0f);
+			{
+				unsigned int newTicks = SDL_GetTicks();
+				if (ticks != newTicks)
+				{
+					int x = 0;
+				}
+			}
+
+			if (elapsed_millis > 1)
+				printf("%d\n", elapsed_millis);
+
+		
+
+			//{
+			//	unsigned int newTicks = SDL_GetTicks();
+			//	if (ticks != newTicks)
+			//	{
+			//		glClearColor(1.0, 0.0, 0.0, 1.0f);
+			//		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			//		agent.Draw();
+			//		SDL_GL_SwapBuffers();
+//
+			//		printf("%d\n", newTicks - ticks);
+			//	}
+			//}
+			
+			//if (elapsed_millis > 0)
+				//printf("%d\n", elapsed_millis);
+		
+			SDL_Delay(1);
 		}
 		else
 		{
 			SDL_Delay(1);
 		}
+		
+*/
 		
 
 		{
